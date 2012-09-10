f11798b205ca45dc6b8c1a86f5cf4919cdf5ae44
Jarrod Bell <jarrod@commandfusion.com>
2012-09-10 16:58:07 +1000

    Cleaned up animated icon demo
    Signed-off-by: Jarrod Bell <jarrod@commandfusion.com>

--- Animated Icon/AnimIcon.js
+++ Animated Icon/AnimIcon.js
@@ -1,11 +1,11 @@
 /*  Muliple frame and Single frame, rotation based animation module for CommandFusion
 ===============================================================================
 
-AUTHOR:		Jarrod Bell, CommandFusion
+AUTHORS:	Jarrod Bell, Florent Pillet - CommandFusion
 CONTACT:	support@commandfusion.com
 URL:		https://github.com/CommandFusion/DemoUserInterfaces
-VERSION:	v2.0.0
-LAST MOD:	17-Dec-2011
+VERSION:	v2.1
+LAST MOD:	22-Feb-2012
 
 =========================================================================
 HELP:
@@ -44,7 +44,7 @@ var AnimIcon = function(params) {
 		totalFrames:	0,							// REQUIRED
 		fps:			5,							// REQUIRED
 		join:			0,							// REQUIRED
-		digitalJoin:	0,							// OPTIONAL
+		triggerJoin:	0,							// OPTIONAL
 		direction:		1,							// OPTIONAL
 		loop:			true,						// OPTIONAL
 		bounce:			false,						// OPTIONAL
@@ -56,7 +56,7 @@ var AnimIcon = function(params) {
 	// direction = 1 for increment, -1 for decrement (default 1)
 	// loop = boolean, true or false, for if you want the animation to continue looping until forced to stop (default to true)
 	// bounce = boolean, true or false, for if you want a looping animation to reverse at each end instead of loop back to start (default false);
-	self.startAnim = function(direction, loop, bounce) {
+	self.start = function(direction, loop, bounce) {
 		if (direction === undefined) {
 			direction = 1;
 		}
@@ -70,97 +70,95 @@ var AnimIcon = function(params) {
 		self.direction = direction;
 
 		// Stop any existing animation going on
-		self.stopAnim(true);
+		self.stop(true);
 
 		// Start the animation
 		self.interval = setInterval(function() {
 			// Set the serial join to the new frame filename
 			CF.setJoin("s"+self.join, self.generateFileName());
+			var dir = self.direction;
+			var total = self.totalFrames;
+			var frame = self.currentFrame;
+
 			// Increment and wrap around the frame number
-			if (self.direction > 0) {
-				if (self.currentFrame+1 > self.totalFrames && loop) {
+			if (dir > 0) {
+				if (frame+1 > total && loop) {
 					// Reached the end of a looping animation
 					if (bounce) {
 						// Reverse the direction
-						self.direction = self.direction*-1;
-						self.currentFrame = self.totalFrames - 1;
+						dir = -dir;
+						frame = total - 1;
 					} else {
-						self.currentFrame = 1;
+						frame = 1;
 					}
 				} else {
-					self.currentFrame++;
+					frame++;
+				}
+			} else if (frame < 2 && loop) {
+				// Reached the end of a looping animation
+				if (bounce) {
+					// Reverse the direction
+					dir = -dir;
+					frame = 2;
+				} else {
+					frame = total;
 				}
 			} else {
-				if (self.currentFrame-1 < 1 && loop) {
-					// Reached the end of a looping animation
-					if (bounce) {
-						// Reverse the direction
-						self.direction = self.direction*-1;
-						self.currentFrame = 2;
-					} else {
-						self.currentFrame = self.totalFrames;
-					}
-				} else {
-					self.currentFrame--;
-				}
+				frame--;
 			}
 
 			// Stop the animation if we get back to 1 and not looping
-			if (((self.direction > 0 && self.currentFrame == self.totalFrames+1) || (self.direction < 0 && self.currentFrame == 0)) && !loop) {
-				if (self.currentFrame > self.totalFrames) {
-					self.currentFrame = self.totalFrames;
-				}
-				if (self.currentFrame < 1) {
-					self.currentFrame = 1;
-				}
+			if (!loop && ((dir > 0 && frame === total+1) || (dir < 0 && frame === 0))) {
+				frame = Math.max(1, Math.min(frame, total));
 				clearInterval(self.interval);
 				// Set the digital join of this animation low because it has finished animating
-				if (self.digitalJoin > 0) {
-					CF.getJoin("d"+self.digitalJoin, function(j,v) {
+				if (self.triggerJoin > 0) {
+					CF.getJoin("d"+self.triggerJoin, function(j,v) {
 						if (v == 1) {
-							CF.setJoin("d"+self.digitalJoin, 0);
+							CF.setJoin("d"+self.triggerJoin, 0);
 						}
 					});
 				}
 			}
+
+			self.currentFrame = frame;
+			self.direction = dir;
 		}, 1000/self.fps);
 	};
 
-	self.stopAnim = function(clearJoin) {
+	self.stop = function(clearJoin) {
 		// Stop any existing animation going on
 		if (self.interval !== null) {
 			clearInterval(self.interval);
 		}
 		// Set the digital join of this animation low because it has finished animating
-		if (self.digitalJoin > 0 && clearJoin != true) {
-			CF.getJoin("d"+self.digitalJoin, function(j,v) {
+		if (clearJoin === true && self.triggerJoin > 0) {
+			CF.getJoin("d"+self.triggerJoin, function(j,v) {
 				if (v == 1) {
-					CF.setJoin("d"+self.digitalJoin, 0);
+					CF.setJoin("d"+self.triggerJoin, 0);
 				}
 			});
 		}
 	};
 
 	self.generateFileName = function() {
-		var frameName = "";
 		var matches = self.regex.exec(self.fileName);
 		// 0 = baseFileName
 		// 1 = prefix
 		// 2 = #'s
 		// 3 = suffix
-		frameName = matches[1]+("00000"+self.currentFrame).slice(matches[2].length*-1)+matches[3];
+		var frameName = matches[1]+("00000"+self.currentFrame).slice(matches[2].length*-1)+matches[3];
 		if (CF.debug) {
 			CF.log("Animation filename: " + frameName);
 		}
-		
 		return frameName;
 	};
 
-	self.digitalJoinTrigger = function(j,v,t) {
+	self.triggerJoinChanged = function(j,v) {
 		if (v == 1) {
-			self.startAnim(self.direction, self.loop, self.bounce);
+			self.start(self.direction, self.loop, self.bounce);
 		} else {
-			self.stopAnim();
+			self.stop(false);
 		}
 	};
 
@@ -172,119 +170,153 @@ var AnimIcon = function(params) {
 	self.loop = params.loop;
 	self.bounce = params.bounce;
 
-	if (params.digitalJoin !== undefined) {
-		self.digitalJoin = params.digitalJoin;
-		CF.watch(CF.JoinChangeEvent, "d"+params.digitalJoin, self.digitalJoinTrigger);
+	if (typeof(params.triggerJoin) === "number") {
+		self.triggerJoin = params.triggerJoin;
+		CF.watch(CF.JoinChangeEvent, "d"+params.triggerJoin, self.triggerJoinChanged);
 	}
 
 	return self;
 };
 
-
-/* Single Frame, Rotation-based animation
- *
- * See example at bottom of script on how to use it
- *
+/**
+ * Animation that rotates an object clockwise or counter-clockwise. Params should be an
+ * object with a number of properties that describe the animation to run
+ * See below for REQUIRED and OPTIONAL parameters:
+ * "duration" (number) OPTIONAL, the duration of a full 360° rotation, in seconds. Defaults to 1 second
+ * "join" (string or number) REQUIRED the join of the object to rotate. Can be a string representing a full join (i.e. "s1") or a number that will be converted to a serial object join (i.e 1 for join "s1"). Image objects are on serial joins, but other object types can be rotated too, like buttons (using their digital join)
+ * "trigger" (string or number) OPTIONAL
+ * @param params    the animation parameters
  */
-var AnimRotation = function(params) {
+var SpinningAnimation = function(params) {
 	var self = {
-		rotationTime:	params.rotationTime || 1,	// OPTIONAL (seconds)
-		join:			params.serialJoin || 0,		// REQUIRED (serial join of image object)
-		digitalJoin:	params.digitalJoin || 0,	// OPTIONAL
+		duration:	    params.duration || 1,	    // OPTIONAL (seconds, defaults to 1 second for full rotation)
+		join:			makeJoinString("s", params.join),
+		triggerJoin:	makeJoinString("d", params.triggerJoin),
 		direction:		params.direction || 1,		// OPTIONAL (1 = clockwise, -1 = anticlockwise)
-		loop:			params.loop || true,		// OPTIONAL
-		bounce:			params.bounce || false,		// OPTIONAL
-		nextAngle:		0
+		loop:           (params.loop === true),     // OPTIONAL (true=loop animation, defaults to false)
+		bounce:         (params.bounce === true),   // OPTIONAL (true=bounce animation, defaults to false)
+		nextAngle:		0,
+		running: 		false
 	};
 
-	// direction = 1 for increment, -1 for decrement (default 1)
-	// loop = boolean, true or false, for if you want the animation to continue looping until forced to stop (default to true)
-	// bounce = boolean, true or false, for if you want a looping animation to reverse at each end instead of loop back to start (default false)
-	self.startAnim = function (direction, loop, bounce) {
-		self.nextAngle = 179 * direction;
-		self.doAnim(direction, loop, bounce);
+	/**
+	 * Start a rotation animation. It is possible to modify the animation parameters that
+	 * were set at construction time by passing direction, loop and bounce. All are optional.
+	 * @param direction     OPTIONAL, 1 for clockwise rotation, -1 for counter-clockwise rotation
+	 * @param loop          OPTIONAL, true if the animation should loop over and over, false for one-shot. Defaults to false
+	 * @param bounce        OPTIONAL, true if the animation should bounce (reverse) when full rotation has been achieved. Defaults to false.
+	 */
+	self.start = function (direction, loop, bounce) {
+		if (self.running) {
+			self.stop(false);
+		}
+		if (self.join == null) {
+			throw "invalid join to animate";
+		}
+		self.running = true;
+		if (self.triggerJoin != null) {
+			CF.setJoin(self.triggerJoin, 1);
+		}
+		if (typeof(direction) === "number") {
+			self.direction = (direction === 1) ? 1 : -1;
+		}
+		if (typeof(loop) === "boolean") {
+			self.loop = loop;
+		}
+		if (typeof(bounce) === "boolean") {
+			self.bounce = bounce;
+		}
+		CF.setProperties({join:self.join, zrotation:0});
+		self.nextDirection = self.direction;
+		self.nextAngle = 179 * self.direction;
+		playNextAnimation();
 	};
 
-	self.doAnim = function(direction, loop, bounce) {
-		if (direction === undefined) {
-			direction = 1;
-		}
-		if (loop === undefined) {
-			loop = true;
-		}
-		if (bounce === undefined) {
-			bounce = false;
-		}
-
-		// Remember the direction no matter how this animation object is triggered (loop and bounce are not remembered across anims)
-		self.direction = direction;
-
-		// Start the animation
-		CF.setProperties({join: "s"+self.join, zrotation: self.nextAngle}, 0, self.rotationTime / 2, CF.AnimationCurveLinear, function() {
-			// Stop the animation if we get back to start and not looping
-			if (self.nextAngle >= 358 && !loop) {
-				// Set the digital join of this animation low because it has finished animating
-				if (self.digitalJoin > 0) {
-					CF.getJoin("d"+self.digitalJoin, function(j,v) {
-						if (v == 1) {
-							CF.setJoin("d"+self.digitalJoin, 0);
-						}
-					});
-				}
-			} else if ((self.nextAngle >= 358 || self.nextAngle <= 2) && bounce) {
-				// bounce the animation
-				direction *=-1
-				self.nextAngle += (direction > 0) ? 179 : -179;
-				self.doAnim(direction, loop, bounce);
-			} else {
-				// continue the animation
-				self.nextAngle += (direction > 0) ? 179 : -179;
-				self.doAnim(direction, loop, bounce);
+	/**
+	 * Stop an ongoing rotation animation. Can optionally clear the digital join used
+	 * to start/stop the animation
+	 * @param resetTrigger     true to update the animation control digital join. Defaults to false.
+	 */
+	self.stop = function(resetTrigger) {
+		if (self.running) {
+			self.running = false;
+			if (self.triggerJoin != null && arguments.length > 0 && resetTrigger) {
+				CF.setJoin(self.triggerJoin, 0);
 			}
-			
-		});
-	};
-
-	self.stopAnim = function(clearJoin) {
-		// Stop any existing animation going on
-		CF.getProperties(self.join, function(j){
-			CF.setProperties({join: "s"+self.join, zrotation: j.zrotation});
-			self.nextAngle = j.zrotation;
-		});
-		// Set the digital join of this animation low because it has finished animating
-		if (self.digitalJoin > 0 && clearJoin != true) {
-			CF.getJoin("d"+self.digitalJoin, function(j,v) {
-				if (v == 1) {
-					CF.setJoin("d"+self.digitalJoin, 0);
-				}
-			});
 		}
 	};
 
-	self.digitalJoinTrigger = function(j,v,t) {
+	/**
+	 * Internal helper to generate a join string. This function ensures that no
+	 * object on join 0 can be used, and accepts numbers (in this case, generates
+	 * a join using type + number) and strings (doesn't modify the given string)
+	 * @param type	a type string "s", "d" or "a" for example
+	 * @param join	a string or number
+	 */
+	function makeJoinString(type, join) {
+		var t = typeof(join);
+		if (t === "number") {
+			return type + join;
+		}
+		if (t === "string") {
+			if (join.length > 0 && join.charAt(0) == type.charAt(0))
+				return join;
+		}
+		return null;
+	}
+
+	function playNextAnimation() {
+		CF.setProperties(   {join: self.join, zrotation: self.nextAngle},
+							0,
+							self.duration / 2,
+							CF.AnimationCurveLinear,
+							function() {
+								if (self.running) {
+									if (!self.loop && Math.abs(self.nextAngle) >= 358) {
+										self.stop(true);
+									} else {
+										if (self.bounce && (self.nextAngle >= 358 || self.nextAngle <= 2)) {
+											self.nextDirection = -self.nextDirection;
+										}
+										self.nextAngle += 179 * self.nextDirection;
+										playNextAnimation();
+									}
+								}
+							});
+	}
+
+	/**
+	 * Private function which starts or stop the animation when the defined trigger join
+	 * changes. If the trigger join is defined, we watch it and this function is the
+	 * watch callback.
+	 */
+	function triggerJoinChanged(j,v) {
 		if (v == 1) {
-			self.doAnim(self.direction, self.loop, self.bounce);
-		} else {
-			self.stopAnim();
+			if (!self.running) {
+				self.start();
+			}
+		} else if (self.running) {
+			self.stop(false);
 		}
-	};
+	}
 
-	if (self.digitalJoin !== undefined) {
-		CF.watch(CF.JoinChangeEvent, "d"+self.digitalJoin, self.digitalJoinTrigger);
+	if (typeof(self.triggerJoin) === "number") {
+		CF.watch(CF.JoinChangeEvent, self.triggerJoin, triggerJoinChanged);
 	}
 
 	return self;
 };
+
 
 // Trigger animations via digital join triggering (buttons in simulation mode, etc) or direct JS calls (button actions, gestures, commands, etc)
 var Anim1 = new AnimIcon({baseFileName: "downlight-##.png", framesPerSecond: 10, totalFrames: 12, serialJoin: 1, digitalJoin: 1, direction: 1, loop: true, bounce: true});
 // Trigger animations via only direct JS calls (button actions, gestures, commands, etc)
 var Anim2 = new AnimIcon({baseFileName: "downlight-##.png", framesPerSecond: 10, totalFrames: 12, serialJoin: 1});
 // Single frame, rotation-based example:
-var Rotation1 = new AnimRotation({rotationTime: 1, serialJoin: 3, direction: 1, loop: true, bounce: true});
+var Rotation1 = new SpinningAnimation({duration: 1, join: 3, direction: 1, loop: true, bounce: false});
 
 // Only one userMain function per project!
 CF.userMain = function () {
 	// Start the loading spinner animation on startup (could also attach to a command triggered by a page timer for same effect)
-	Rotation1.doAnim();
+	Rotation1.start();
 };
