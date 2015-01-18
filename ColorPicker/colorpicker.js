f546869d7117cd8cb7d6c1998dc15165f5590cb6
Jarrod Bell <jarrod@commandfusion.com>
2015-01-19 10:42:53 +1100

    Use browser base64 encoding functions

--- ColorPicker/colorpicker.js
+++ ColorPicker/colorpicker.js
@@ -65,11 +65,11 @@ var ColorPicker = function(url, pickerJoin, hoverJoin, freq, callback) {
 			self.can.height = self.img.height;
 			self.ctx.drawImage(self.img, 0, 0, self.img.width, self.img.height);
 		}
-		
+
 		self.img.crossOrigin = '';
-			
+
 		CF.loadAsset(self.imageURL, CF.BINARY, function (data) {
-			self.img.src = "data:image/png;base64," + encode64(data);
+			self.img.src = "data:image/png;base64," + btoa(data);
 		}, CF.CACHE);
 	};
 
@@ -91,43 +91,6 @@ var ColorPicker = function(url, pickerJoin, hoverJoin, freq, callback) {
 
 	return self;
 };
-
-function encode64(input) {
-	var output = "";
-	var chr1, chr2, chr3 = "";
-	var enc1, enc2, enc3, enc4 = "";
-	var i = 0;
-	
-	var keyStr = "ABCDEFGHIJKLMNOP" +
-                "QRSTUVWXYZabcdef" +
-                "ghijklmnopqrstuv" +
-                "wxyz0123456789+/" +
-                "=";
-
-	do {
-		chr1 = input.charCodeAt(i++);
-		chr2 = input.charCodeAt(i++);
-		chr3 = input.charCodeAt(i++);
-
-		enc1 = chr1 >> 2;
-		enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-		enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-		enc4 = chr3 & 63;
-
-		if (isNaN(chr2)) enc3 = enc4 = 64;
-		else if (isNaN(chr3)) enc4 = 64;
-
-		output = output +
-		keyStr.charAt(enc1) +
-		keyStr.charAt(enc2) +
-		keyStr.charAt(enc3) +
-		keyStr.charAt(enc4);
-		chr1 = chr2 = chr3 = "";
-		enc1 = enc2 = enc3 = enc4 = "";
-	} while (i < input.length);
-
-	return output;
-}
 
 // Push the modules into the startup process
 CF.modules.push({name: "Color Picker", object: ColorPicker});
